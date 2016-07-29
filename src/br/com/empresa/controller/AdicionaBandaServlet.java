a7668f06f725f00583904db26cc9a26110ddae12
phelipeguedes <phelipe.guedes@hotmail.com>
2016-07-29 17:47:43 -0300

    vers. 1.0

--- src/br/com/empresa/controller/AdicionaBandaServlet.java
+++ src/br/com/empresa/controller/AdicionaBandaServlet.java
@@ -2,16 +2,12 @@ package br.com.empresa.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import br.com.empresa.dao.BandaDao;
 import br.com.empresa.modelo.Banda;
 
@@ -20,21 +16,13 @@ import br.com.empresa.modelo.Banda;
 
 public class AdicionaBandaServlet extends HttpServlet {
 
-	@SuppressWarnings("unused")
-	private ServletContext contexto;
-
 	public AdicionaBandaServlet() {
 		super();
 	}
 
 	/* O método service aceita todos os métodos HTTP, ou seja, tanto GET quanto POST */
 
-	public void init(ServletConfig config) {
-		contexto = config.getServletContext();
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String nome = request.getParameter("nome");
 		String genero = request.getParameter("genero");
